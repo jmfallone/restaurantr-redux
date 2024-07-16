04cb7865b3fa1dfcff446ff215fbd7088a20c073
Joey Fallone <j.fallone@ualberta.ca>
2024-07-15 22:28:06 -0600

    feat: basic api setup + users/reviews CRUD

---
+++ api/app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+      before_action :set_review, only: [:show, :update, :destroy]
+
+      def index
+        @reviews = Review.all
+        render json: @reviews
+      end
+
+      def show
+        render json: @review
+      end
+
+      def create
+        @review = Review.new(review_params)
+        if @review.save
+          render json: @review, status: :created
+        else
+          render json: @review.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @review.update(review_params)
+          render json: @review
+        else
+          render json: @review.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @review.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_review
+        @review = Review.find(params[:id])
+      end
+
+      def review_params
+        params.require(:review).permit(:title, :subtitle, :rating, :image, :restaurant_id, :user_id, :restaurant_name)
+      end
+    end
+  end
+end
