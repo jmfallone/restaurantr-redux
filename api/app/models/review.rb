3277840a868942f9c4de28c783f3fb151bfec7ee
Joey Fallone <jm@Joeys-MBP.lan>
2024-09-02 20:07:42 -0600

    feat: backend reviews CRUD

--- api/app/models/review.rb
+++ api/app/models/review.rb
@@ -1,2 +1,15 @@
 class Review < ApplicationRecord
+  validates :creator_username, presence: true
+  validates :date_created, presence: true
+  validates :score, presence: true, inclusion: { in: 1..10 }
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :store_id, presence: true
+
+  serialize :images, JSON
+
+  # Custom setter to ensure images is always an array
+  def images=(value)
+    super(Array(value))
+  end
 end
